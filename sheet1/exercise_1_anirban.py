f9d709c551ef9062eb09b7c3aeda289f829f4ffb
Avocadoakbar <33468224+Avocadoakbar@users.noreply.github.com>
2020-11-14 12:06:17 +0100

    Update exercise_1_anirban.py
    Hi Anirban, I just tried out your code and wrote around a little in it, to see what modules you are working with and wether everything also works for me. Did you have the same problems as I, e.g. with cv.cvtColor? Cheers Paul

--- sheet1/exercise_1_anirban.py
+++ sheet1/exercise_1_anirban.py
@@ -4,6 +4,8 @@
 Created on Mon Nov  9 00:01:50 2020
 
 @author: anirban
+
+annotations and test: paul
 """
 
 import numpy as np
@@ -16,15 +18,19 @@ img = cv.imread("brain.png")
 
 ret, binary_img = cv.threshold(img, 100, 255, cv.THRESH_BINARY)
 cv.imwrite("brain_bin.png", binary_img)
-#plt.show()
+print(ret)
+plt.imshow(binary_img, cmap='gray')
+plt.show()
 
 
 #2)
 gradient = np.linspace(0, 1, num=255)
 image = np.tile(gradient, (100, 1))
-#print(image)
+print(image)
+# not working as original no color image (255x100x3 array)
+# but just an 255x100 array or sth.
 #image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 plt.imshow(image, cmap='gray')
-#plt.savefig("gradient.png", image, cmap="gray")
+plt.savefig("gradient.png")
 plt.show()
-#cv.imwrite("gradient.png", image)
+cv.imwrite("gradient_test.png", image)
